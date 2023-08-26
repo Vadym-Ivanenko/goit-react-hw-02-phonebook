7707936f8a9d722134c265820b238f5bae57f444
Vadym <v.ivanenko000@gmail.com>
2023-08-27 00:34:18 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,66 @@
-export const App = () => {
-  return <div></div>;
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Phonebook } from './phonebook/Phonebook';
+import { Filter } from './filter/Filter';
+import { ContactList } from './contact-list/ContactList';
+import { Wrapper, Title, Subtitle } from './App.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handelChange = evt => {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  };
+
+  addContact = newContact => {
+    this.setState(prevState => ({
+      contacts: [
+        ...prevState.contacts,
+        {
+          id: nanoid(),
+          ...newContact,
+        },
+      ],
+    }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  getFilterContacts = () => {
+    const filteredByName = this.state.contacts.filter(contact => {
+      return contact.name
+        .toLowerCase()
+        .includes(this.state.filter.toLocaleLowerCase());
+    });
+    return filteredByName;
+  };
+
+  render() {
+    const { filter } = this.state;
+    return (
+      <Wrapper>
+        <Title>Phonebook</Title>
+        <Phonebook onAdd={this.addContact} />
+        <Subtitle>Contacts</Subtitle>
+        <Filter filter={filter} handelChange={this.handelChange} />
+        <ContactList
+          contactFilter={this.getFilterContacts()}
+          deleteContact={this.deleteContact}
+        />
+      </Wrapper>
+    );
+  }
+}
